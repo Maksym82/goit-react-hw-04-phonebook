91993caa6a4e35d40c83061e10f9a3adc51d6a5f
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-08-25 14:09:54 +0300

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,39 +1,36 @@
 import React from 'react';
-import { List, Item, Button, Text } from './ContactList.styled'
+import { List, Item, Button, } from './ContactList.styled'
 
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 
 export const  ContactList = ({ contacts, onDeleteContact }) => (
   <List>
     {contacts.map(contact => (
       <Item key={contact.id}>
-        <Text>
         {contact.name + ' : ' + contact.number}
-        </Text>
-        
-        
+        {
           <Button
             type="button"
             name="delete"
             onClick={() => onDeleteContact(contact.id)}
           >
-            Delete
+            delete
           </Button>
-        
+        }
       </Item>
     ))}
   </List>
 );
 
-// ContactList.propTypes = {
-//   contacts: PropTypes.arrayOf(
-//     PropTypes.exact({
-//       id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       number: PropTypes.string.isRequired,
-//     })
-//   ),
-//   onDeleteContact: PropTypes.func.isRequired,
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
 
-// };
+};
